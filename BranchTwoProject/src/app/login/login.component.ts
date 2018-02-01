152a1aadc1c128192c865a4fc2c4d322344b60bf
Kothuru <Ashwini.Kothuru@thomsonreuters.com>
2018-02-01 13:27:18 +0530

    Modified files adding to git repo

--- BranchTwoProject/src/app/login/login.component.ts
+++ BranchTwoProject/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NG_VALIDATORS,Validator,
+  Validators,AbstractControl,ValidatorFn } from '@angular/forms'; 
 
 @Component({
   selector: 'app-login',
@@ -6,18 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email:string ="Ashwini";
+  email:string ="Ashwini.k@g.com";
   password:string ="Ashwini";
-  details: Array<Object> = [{email: 'Ashwini', password: 'Ashwini'},{
-email: 'prasad' ,password:'Prasad' }]; //array defining
-
-  
+  details: Array<Object> = [{email: 'Ashwini.k@g.com', password: 'Ashwini'
+ }]; //array defining
+ isValidFormSubmitted = false;
+ validateEmail = true;
+ emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+ submitted = false;
   constructor() { }
   
   ngOnInit() {
   }
   
-  signIn(){ //this is function 
+  signIn(){ //this is sign-in function 
+    this.submitted = true;
     var data = {email: this.email, password: this.password};
     this.details.push(data);
   }
